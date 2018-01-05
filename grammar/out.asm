b081c576f23fc110eb06165cb44f0ac8973c39e8
Vahid Mohammadi <email@vmoh.ir>
2018-01-06 02:35:20 +0330

    testing

--- grammar/out.asm
+++ grammar/out.asm
@@ -1,37 +1,92 @@
 main:
 move $fp, $sp
-# start syscall 12
-li $v0, 12
-syscall
-# end syscall
-addiu $a0, $v0, 0
+#### addVariableToStack -- adding a number to stack
+li $a0, 0
 sw $a0, 0($sp)
 addiu $sp, $sp, -4
-# start syscall 12
-li $v0, 12
-syscall
-# end syscall
-addiu $a0, $v0, 0
+li $a0, 0
 sw $a0, 0($sp)
 addiu $sp, $sp, -4
-addi $a0, $zero, 10
-# start syscall 11
-li $v0, 11
-syscall
-# end syscall
+#### addVariableToStack -- end of adding a number to stack
 # pop stack
 addiu $sp, $sp, 8
 # end of pop stack
+# start of adding address to stack
+addiu $a0, $fp, 0
+sw $a0, 0($sp)
+addiu $sp, $sp, -4
+addiu $a0, $fp, -4
+sw $a0, 0($sp)
+addiu $sp, $sp, -4
+# end of adding address to stack
 #### addVariableToStack -- adding a number to stack
 li $a0, 1
 sw $a0, 0($sp)
 addiu $sp, $sp, -4
 #### addVariableToStack -- end of adding a number to stack
+#### addVariableToStack -- adding a number to stack
+li $a0, 2
+sw $a0, 0($sp)
+addiu $sp, $sp, -4
+#### addVariableToStack -- end of adding a number to stack
+# start of assign
+lw $a0, 4($sp)
+# pop stack
+addiu $sp, $sp, 4
+# end of pop stack
+lw $a1, 8($sp)
+sw $a0, 0($a1)
+sw $a0, 8($sp)
+lw $a0, 4($sp)
+# pop stack
+addiu $sp, $sp, 4
+# end of pop stack
+lw $a1, 8($sp)
+sw $a0, 0($a1)
+sw $a0, 8($sp)
+# end of assign
+# start of adding variable to stack
+lw $a0, 0($fp)
+sw $a0, 0($sp)
+addiu $sp, $sp, -4
+lw $a0, -4($fp)
+sw $a0, 0($sp)
+addiu $sp, $sp, -4
+# end of adding variable to stack
+# pop stack
+addiu $sp, $sp, 8
+# end of pop stack
+#### addVariableToStack -- adding a number to stack
+li $a0, 0
+sw $a0, 0($sp)
+addiu $sp, $sp, -4
+#### addVariableToStack -- end of adding a number to stack
+# start of adding address to stack
+addiu $a0, $fp, 0
+sw $a0, 0($sp)
+addiu $sp, $sp, -4
+# end of adding address to stack
+# access array
+lw $a0, 4($sp)
+# pop stack
+addiu $sp, $sp, 4
+# end of pop stack
+lw $a1, 4($sp)
+# pop stack
+addiu $sp, $sp, 4
+# end of pop stack
+li $t0, 4
+mul $a1, $a1, $t0
+sub $a0, $a0, $a1
+lw $a0, 0($a0)
+sw $a0, 0($sp)
+addiu $sp, $sp, -4
+# end access array
 # writing
 # writeone
 lw $a0, 4($sp)
-# start syscall 11
-li $v0, 11
+# start syscall 1
+li $v0, 1
 syscall
 # end syscall
 # end writeone
@@ -45,7 +100,7 @@ syscall
 # end syscall
 # end of writing
 # pop stack
-addiu $sp, $sp, 0
+addiu $sp, $sp, 8
 # end of pop stack
 # pop stack
 addiu $sp, $sp, 0
